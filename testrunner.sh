68829d289b35c6211e038cb59c3a6ba4742d06a8
Martin Prebio <mp@25th-floor.com>
2013-11-16 01:06:49 +0100

    Modifiying testrunner to support several different test classes, as well as adding the battery

--- testrunner.sh
+++ testrunner.sh
@@ -3,15 +3,19 @@
 BIN='./dlv.i386-apple-darwin-iodbc'
 OPTS='-N=75 component_tester.dl -filter=UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p'
 
-for TEST in g.test* ;
+for CLASS in g b;
 do
-	RES=`eval "$BIN $OPTS g.dl $TEST" | sed -e 's/DLV \[build .*//'`
-	COUNT=`eval echo '$RES' | wc -c`
-	echo -n $TEST "..... "
-	if [ $COUNT -eq 3 ]; then
-		echo "OK"
-	else
-		echo "FAILED"
-		echo $RES
-	fi
-done
+	echo "----- Test class $CLASS ----"
+	for TEST in $CLASS.test* ;
+	do
+		RES=`eval "$BIN $OPTS $CLASS.dl $TEST" | sed -e 's/DLV \[build .*//'`
+		COUNT=`eval echo '$RES' | wc -c`
+		echo -n $TEST "..... "
+		if [ $COUNT -eq 3 ]; then
+			echo "OK"
+		else
+			echo "FAILED"
+			echo $RES
+		fi
+	done
+done;
