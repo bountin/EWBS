96fe832c78a53f853cc75a8e4ac206276f49ecff
Martin Prebio <mp@25th-floor.com>
2013-11-29 01:12:26 +0100

    Adapting testrunner for system tests

--- testrunner.sh
+++ testrunner.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 BIN='./dlv.i386-apple-darwin-iodbc'
-OPTS='-N=75 component_tester.dl -filter=UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p'
+OPTS='-N=175 -filter=UNCOMPUTED_c,UNCOMPUTED_p,UNEXPECTED_c,UNEXPECTED_p,DUPLICATED_c,DUPLICATED_p'
 
-for CLASS in g b s;
+for CLASS in g b s system;
 do
+	if [ "$CLASS" == "system" ]; then
+		TESTER='system_tester.dl connect.dl s.dl g.dl b.dl'
+	else
+		TESTER='component_tester.dl $CLASS.dl'
+	fi
+
 	echo "----- Test class $CLASS ----"
 	for TEST in $CLASS.test* extTests/*/$CLASS.test*;
 	do
-		RES=`eval "$BIN $OPTS $CLASS.dl $TEST" | sed -e 's/DLV \[build .*//'`
+		RES=`eval "$BIN $OPTS $TESTER $TEST" | sed -e 's/DLV \[build .*//'`
 		COUNT=`eval echo '$RES' | wc -c`
 		echo -n $TEST "..... "
 		if [ $COUNT -eq 3 ]; then
